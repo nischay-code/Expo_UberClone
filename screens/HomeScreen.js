870fbd06d1bfe098c8a40531014935588279ba56
Nischay Patel <95124327+nischay-code@users.noreply.github.com>
2022-03-19 19:18:53 +0530

    First Commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import { StyleSheet, View, SafeAreaView, Image } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import NavOptions from "../slices/components/NavOptions";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={[tw`bg-white h-full`]}>
+      <View style={[tw`p-5`]}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default HomeScreen;
